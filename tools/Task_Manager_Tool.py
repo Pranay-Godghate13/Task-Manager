ec082d1e56ee0543b05f46a874b75d50e7efee33
Pranay <pranaygodghate13@gmail.com>
2025-12-31 22:57:41 +0530

    Db implemented

--- tools/Task_Manager_Tool.py
+++ tools/Task_Manager_Tool.py
@@ -1,16 +1,50 @@
 import sqlite3
-a=[]
+from crew.ai import tools
+
+def init_db():
+    conn=sqlite3.connect("tasks.db")
+    cursor=conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tasks(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   task TEXT NOT NULL,
+                   date TEXT NOT NULL)
+""")
+    conn.commit()
+    conn.close()
+
+@tools("Add tasks to database")
 def add_task_to_db(task: str,due_date: str):
-    a.append({"task":task,"date":due_date})
-    #print("Task completed sucessfully.")
+    conn=sqlite3.connect("tasks.db")
+    cursor=conn.cursor()
+    cursor.execute("INSERT INTO tasks(task,date) values(?,?)",(task,due_date))
+    conn.commit()
+    conn.close()
 
+@tools("List all the tasks")
 def list_task():
-    for i,elem in enumerate(a):
-        print(f"Task is : {elem['task']} | Due date is : {elem['date']}")
+    conn=sqlite3.connect("tasks.db")
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM tasks")
+    for row in cursor.fetchall():
+        print(f"{row[0]}. Task: {row[1]} | Date: {row[2]}")
+    conn.close()
 
+@tools("Delete tasks")
 def delete_task(task_id: int):
-    a.pop(task_id-1)
-    #print("Deleted successfully.")
+    conn=sqlite3.connect("tasks.db")
+    cursor=conn.cursor()
+    cursor.execute("DELETE FROM tasks where id=?",(task_id,))
+    conn.commit()
+    conn.close()
 
+#init_db()
+
+# Usage
+#add_task_to_db("Wake me up at 6 AM", "31/12/2025")
+#delete_task(2)
+#list_task()
+#delete_task(1)
+#list_task()
 
 
